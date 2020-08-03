77da5fa7377ed9fa092c2e514afb78b2f8dd9e39
tibaredha <tibaredha@yahoo.fr>
2020-08-03 15:09:12 +0100

    add index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<?php 
-require  'vendor/autoload.php';  
-require  'libs/Config.php';
+<?php
+require './libs/config.php';
+// $cfg = './libs/cfgg.php';
+$cfg = './libs/cfg.php';
+if(!file_exists($cfg)) {header('location: ./install/');}else {require $cfg;} 
 spl_autoload_register(function ($class) {require LIBS . $class .".php";});
 $app = new Bootstrap();
 ?>
